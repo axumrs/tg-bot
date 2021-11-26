e58cad4bea58e1e01155192df389733c64b4c099
axumrs <rolson867@gmail.com>
2021-11-26 12:18:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# 使用axum开发telegram机器人
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "tg_bot=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new();
+
+    axum::Server::bind(&"127.0.0.1:9527".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
